caab432abf596d0d80a906b53ba55856eeac09e0
Joël <mail@joelbosch.nl>
2025-02-03 21:29:34 +0100

    Setup for run command

---
+++ server/commands/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+################################################################################
+#
+#   Run
+#
+#       This bash file runs all commands to setup up a local development
+#       environment for the server.
+#
+################################################################################
+
+current_directory=$(pwd)
+server_root_directory="${current_directory%/*}"
+infrastructure_directory="$server_root_directory/src/infrastructure"
+
+
+echo "TBD: Run the server"
