67b86d8509483ed1347191046179caad6b7222ab
Joël <mail@joelbosch.nl>
2025-08-23 23:21:21 +0200

    No longer need NewCfnOutput interface

--- server/infrastructure/interfaces/NewCfnOutput.go
+++
@@ -1,8 +0,0 @@
-package interfaces
-
-import (
-	"github.com/aws/aws-cdk-go/awscdk/v2"
-	"github.com/aws/constructs-go/constructs/v10"
-)
-
-type NewCfnOutput func(scope constructs.Construct, id *string, props *awscdk.CfnOutputProps) awscdk.CfnOutput
